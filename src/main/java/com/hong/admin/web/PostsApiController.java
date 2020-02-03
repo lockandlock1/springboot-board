00d95b692ab8c0e67e1cd0a50b0ab613fda9316c
redhong <hongstar2311@gmail.com>
2020-02-04 00:25:39 +0900

    init commit

--- src/main/java/com/hong/admin/web/PostsApiController.java
+++ src/main/java/com/hong/admin/web/PostsApiController.java
@@ -3,6 +3,7 @@ package com.hong.admin.web;
 import com.hong.admin.service.posts.PostsService;
 import com.hong.admin.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostsApiController {
     private final PostsService postsService;
 
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
